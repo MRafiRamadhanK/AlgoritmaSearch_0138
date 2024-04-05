3fc2db1d152b2f197f9bb1d3053f1854901f8d56
MRafiramadhanK <raficool271003@gmail.com>
2024-04-05 09:08:06 +0700

    pelengkap void input

--- AlgoritmaSearch_0138/AlgoritmaSearch_0138.cpp
+++ AlgoritmaSearch_0138/AlgoritmaSearch_0138.cpp
@@ -13,5 +13,17 @@ void input
 		cin >> n;
 		if ((n > 0) && (n <= 20))
 			break;
+		else
+			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
 	}
+
+	cout << "\n----------------------\n";
+	cout << "Enter Element Array\n";
+	cout << "------------------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> att[i];
+	}
+
 }
